a81c4d4713de57d7584f75da8a7cfde114ebc0e9
Luisa Arboleda <luifa07@gmail.com>
2024-10-16 19:02:40 -0500

    clase 28

---
+++ clase28/src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './pages/App'
+
+import { Provider } from "react-redux";
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
